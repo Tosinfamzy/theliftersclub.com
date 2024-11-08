1bff5a0c798d9db12f074e772afa800694a6dee0
Tosin Famurewa <famurewatosin@gmail.com>
2024-11-08 00:42:37 +0000

    bug fix for vercel cleanup

--- src/hooks/useExercises.ts
+++ src/hooks/useExercises.ts
@@ -7,6 +7,7 @@ export const useExercises = () => {
   return useQuery<Excercise[]>({
     queryKey: ["exercises"],
     queryFn: async () => {
+      console.log(import.meta.env.VITE_API_URL);
       const { data } = await axios.get("/excercises");
       return data;
     },
