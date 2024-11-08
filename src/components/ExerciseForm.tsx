d6e2cba56b423998ec7191f28379a30d1aa06805
Tosin Famurewa <famurewatosin@gmail.com>
2024-11-08 00:51:47 +0000

    clean up

--- src/components/ExerciseForm.tsx
+++ src/components/ExerciseForm.tsx
@@ -61,19 +61,6 @@ const ExerciseForm = () => {
                 </div>
               </div>
             ))}
-
-          {/* <div className="flex flex-col items-center justify-between w-full p-6 bg-white rounded-lg md:flex-row">
-            <p className="font-bold text-center text-veryDarkViolet md:text-left">
-              https://frontendmentor.io
-            </p>
-
-            <div className="flex flex-col items-center justify-end flex-1 space-x-4 space-y-2 md:flex-row md:space-y-0">
-              <div className="font-bold text-cyan">https://rel.ink/k4IKyk</div>
-              <button className="p-2 px-8 text-white bg-cyan rounded-lg hover:opacity-70 focus:outline-none">
-                Copy
-              </button>
-            </div>
-          </div> */}
         </div>
       </section>
     </>
--- src/hooks/useExercises.ts
+++ src/hooks/useExercises.ts
@@ -7,7 +7,6 @@ export const useExercises = () => {
   return useQuery<Excercise[]>({
     queryKey: ["exercises"],
     queryFn: async () => {
-      console.log(import.meta.env.VITE_API_URL);
       const { data } = await axios.get("/excercises");
       return data;
     },
